7138386ab3c6b01fe376788fc99622637b96905d
“Jessie” <jessc.moyano@gmail.com>
2024-07-23 17:52:18 +0800

    Fix the export function of nav object ps1

--- Export Objects.ps1
+++ Export Objects.ps1
@@ -1,106 +1,134 @@
 function Export-Nav-Objects {
     param (
-        # Parameter help description
+        # Project Path
+        [Parameter(Mandatory = $true)]
+        [string]
+        $Path,
 
+        # Database Server Name
+        [Parameter(Mandatory = $true)]
+        [string]
+        $ServerName,
+
+        # Database Username
+        [Parameter(Mandatory = $true)]
+        [string]
+        $Username,
+
+        # Database Password
+        [Parameter(Mandatory = $true)]
+        [string]
+        $Password,
+
+        # Parameter help description
         [Parameter(Mandatory = $true)]
         [string]
         $VersionList,
+
+        # Database name of development
+        [Parameter(Mandatory = $true)]
+        [string]
+        $DatabaseNameDev,
 
         [Parameter(Mandatory = $false)]
         [boolean]
         $ExportDevFob,
 
+        # Database name of Live
         [Parameter(Mandatory = $false)]
-        [boolean]
-        $ExportLiveFob
+        [string]
+        $DatabaseNameLive,
+
+        # RunAsDate Exe File
+        [Parameter(Mandatory = $true)]
+        [String]
+        $RunAsDateExe,
+
+        # FinSQL Exe File
+        [Parameter(Mandatory = $true)]
+        [String]
+        $FinSqlExe
     )
+    #Check if Version List has corresponding result
+    $SelectQuery = "SELECT COUNT(ID) AS Result FROM [dbo].[Object] WHERE [Version List] LIKE '%$VersionList%'"
+    $result = Invoke-Sqlcmd -ServerInstance $ServerName -Database $DatabaseNameDev -Password $Password -Username $Username -Query $SelectQuery
+    if($result.Result -eq 0){
+        Throw "There's no Version List found using the Version $VersionList" 
+    }
+    #Check if project path is valid
+    if (!(Test-Path $Path)) { Throw "$Path path cannot be found." }
 
-    $Location = "D:\Development\Dynamics NAV\NAVOC1" # Working Path
-    $ParentPath = "$Location\$VersionList"
-    $ServerName = "172.16.1.221" # Database Server of Development and Live
-    $DatabaseNameDev = "NAV2016_DEV3" #database name of Non-production
-    $DatabaseNameLive = "NAV2016" #database name of Production
-    $DatabaseUsername = "sa" #Username
-    $DatabasePassword = "Password1." #Password
+    # New-Item $Path -Name "Development"  -ItemType Directory -Force
+    New-Item $Path -Name "Fob"  -ItemType Directory -Force
+    New-Item $Path -Name "Logs"  -ItemType Directory -Force
 
-    # Do not change the below declaration
-    $ObjectFilter = "Version List=*$VersionList*"
-    $LogFileDev = "`"$ParentPath\QueryExportLogTxt.txt`""
-    $FileDevTxt = "$ParentPath\1 DEV_$VersionList.txt"
-    $FileDevFob = "$ParentPath\2 DEV_$VersionList.Fob"
-    $FileLiveTxt = "$ParentPath\1 LIVE_$VersionList.txt"
-    $FileLiveFob = "$ParentPath\2 LIVE_$VersionList.Fob"
-    
     #Check if RunAsDate file
-    $RunAsDateExeFile = "D:\Program Files\RunAsDate\RunAsDate.exe"
-    if (!(Test-Path $RunAsDateExeFile)) { Throw "$RunAsDateExeFile RunAsDate.exe file cannot be found" }
+    if (!(Test-Path $RunAsDateExe)) { Throw "$RunAsDateExe RunAsDate.exe file cannot be found." }
     
     # Check finsql.exe file
-    $FinSQLExeFile = "C:\Program Files (x86)\Microsoft Dynamics NAV\90\RoleTailored Client\finsql.exe"
-    if (!(Test-Path $FinSQLExeFile)) { Throw "$FinSQLExeFile finsql.exe file cannot be found" }
+    if (!(Test-Path $FinSqlExe)) { Throw "$FinSqlExe finsql.exe file cannot be found." }
 
-    #$DeltaFile = "$ParentPath\0 DELTAFILE_$VersionList.txt"
-    $ExportSript = "`"$RunAsDateExeFile`" /movetime 26\06\2018 00:00:00 "
-    $ExportSript += "`"$FinSQLExeFile`" command=exportobjects, "
-    $ExportSript += "servername=$ServerName, username=$DatabaseUsername, password=$DatabasePassword, filter=$ObjectFilter,"
-
-
-    #Update the Version List from Development to Live
-    $SelectQuery = "SELECT * FROM [dbo].[Object] WHERE [Version List] LIKE '%$VersionList%'"
-    $result = Invoke-Sqlcmd -ServerInstance $ServerName -Database $DatabaseNameDev -Password $DatabasePassword -Username $DatabaseUsername -Query $SelectQuery #-Encrypt Optional 
-
-    # Create new file for object list
-    $ObjectList = "$ParentPath\Objects.txt" # Object list
-    if (!(Test-Path $ObjectList)) {
-        New-Item $ObjectList # Creating new text file
-    } else {
-        Clear-Content $ObjectList # Removing text value
-    }
-
-    foreach ($Object in $result) {
-        $VLVersionList = $Object."Version List"
-        $VLType = $Object.Type
-        $VLID = $Object.ID
-        $VLName = $Object.Name
-
-        # Update the Version List of Object on the Live Database
-        $UpdateQuery = "UPDATE Object SET [Version List] = '$VLVersionList' WHERE Type = $VLType AND ID = $VLID"
-        Invoke-Sqlcmd -ServerInstance $ServerName -Database $DatabaseNameLive -Password $DatabasePassword -Username $DatabaseUsername -Query $UpdateQuery #-Encrypt Optional 
-
-        switch ($Object.Type) {
-            1 #Table
-            {$NAVObjectTypeName = "Table"}
-            3 #Report
-            {$NAVObjectTypeName = "Report"}
-            5 #Codeunit
-            {$NAVObjectTypeName = "Codeunit"}
-            6 #XMLPort
-            {$NAVObjectTypeName = "XMLPort"}
-            7 #MenuSuite
-            {$NAVObjectTypeName = "MenuSuite"}
-            8 #Page
-            {$NAVObjectTypeName = "Page"}
-            9 #Query
-            {$NAVObjectTypeName = "Query"}
-            Default {}
-        }
-        #create Text File for the Object List
-        if ((Test-Path $ObjectList) -and ($Object.Type -ne 0)) {
-            $NAVObjectName = "$VLType $NAVObjectTypeName $VLID $VLName $VLVersionList"
-            Add-Content -Path $ObjectList -Value $NAVObjectName
-        }
-    }
-
+    #$DeltaFile = "$Path\0 DELTAFILE_$VersionList.txt"
+    $ExportSript = "`"$RunAsDateExe`" /movetime 26\06\2018 00:00:00 "
+    $ExportSript += "`"$FinSqlExe`" command=exportobjects, "
+    $ExportSript += "servername=$ServerName, username=$Username, password=$Password, filter=Version List=$VersionList,"
 
     #Export NAV Objects Command
-    & cmd /c "$ExportSript database=$DatabaseNameDev, file=$FileDevTxt, ntauthentication=no, logfile=$LogFileDev"
-    # Allow Export DevFob
-    if ($ExportDevFob) { & cmd /c "$ExportSript database=$DatabaseNameDev, file=$FileDevFob, ntauthentication=no, logfile=$LogFileDev" }
-    
-    & cmd /c "$ExportSript database=$DatabaseNameLive, file=$FileLiveTxt, ntauthentication=no, logfile=$LogFileDev"
-    # Allow Export LiveFob
-    if ($ExportLiveFob) { & cmd /c "$ExportSript database=$DatabaseNameLive, file=$FileLiveFob, ntauthentication=no, logfile=$LogFileDev" }
+    & cmd /c "$ExportSript database=$DatabaseNameDev, file=$Path\DEV $VersionList.txt, ntauthentication=no, logfile=$Path\Logs\DEV $VersionList.txt"
+    if ($ExportDevFob) { # Allow Export DevFob
+        & cmd /c "$ExportSript database=$DatabaseNameDev, file=$Path\Fob\DEV $VersionList.fob, ntauthentication=no" 
+    }
 
+    #Update the Version List from Development to Live
+    if ($DatabaseNameLive -ne "") {
+        $SelectQuery = "SELECT * FROM [dbo].[Object] WHERE [Version List] LIKE '%$VersionList%'"
+        $result = Invoke-Sqlcmd -ServerInstance $ServerName -Database $DatabaseNameDev -Password $Password -Username $Username -Query $SelectQuery #-Encrypt Optional 
+
+        # # Create new file for object list
+        # $ObjectList = "$Path\Objects.txt" # Object list
+        # if (!(Test-Path $ObjectList)) {
+        #     New-Item $ObjectList # Creating new text file
+        # } else {
+        #     Clear-Content $ObjectList # Removing text value
+        # }
+
+        foreach ($Object in $result) {
+            $VLVersionList = $Object."Version List"
+            $VLType = $Object.Type
+            $VLID = $Object.ID
+            $VLName = $Object.Name
+
+            # Update the Version List of Object on the Live Database
+            $UpdateQuery = "UPDATE Object SET [Version List] = '$VLVersionList' WHERE Type = $VLType AND ID = $VLID"
+            Invoke-Sqlcmd -ServerInstance $ServerName -Database $DatabaseNameLive -Password $Password -Username $Username -Query $UpdateQuery #-Encrypt Optional 
+
+            # switch ($Object.Type) {
+            #     1 #Table
+            #     {$NAVObjectTypeName = "Table"}
+            #     3 #Report
+            #     {$NAVObjectTypeName = "Report"}
+            #     5 #Codeunit
+            #     {$NAVObjectTypeName = "Codeunit"}
+            #     6 #XMLPort
+            #     {$NAVObjectTypeName = "XMLPort"}
+            #     7 #MenuSuite
+            #     {$NAVObjectTypeName = "MenuSuite"}
+            #     8 #Page
+            #     {$NAVObjectTypeName = "Page"}
+            #     9 #Query
+            #     {$NAVObjectTypeName = "Query"}
+            #     Default {}
+            # }
+            # #create Text File for the Object List
+            # if ((Test-Path $ObjectList) -and ($Object.Type -ne 0)) {
+            #     $NAVObjectName = "$VLType $NAVObjectTypeName $VLID $VLName $VLVersionList"
+            #     Add-Content -Path $ObjectList -Value $NAVObjectName
+            # }
+        }
+        
+        & cmd /c "$ExportSript database=$DatabaseNameLive, file=$Path\LIVE $VersionList.txt, ntauthentication=no, logfile=$Path\Logs\LIVE $VersionList.txt"
+        & cmd /c "$ExportSript database=$DatabaseNameLive, file=$Path\Fob\LIVE $VersionList.fob, ntauthentication=no"
+    }
 
 # if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 # Set-ExecutionPolicy unrestricted -Force
